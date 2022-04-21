70fe7243f73b4124e4e88da6d39616993e911044
JOWIC <dev@arrowtrade.ch>
2022-04-21 23:17:36 +0200

    Update masternode-sync.cpp

--- src/masternode-sync.cpp
+++ src/masternode-sync.cpp
@@ -31,13 +31,13 @@ bool CMasternodeSync::IsSynced()
 
 bool CMasternodeSync::IsBlockchainSynced()
 {
-    static bool fBlockchainSynced = false;
+    static bool fBlockchainSynced = true;
     static int64_t lastProcess = GetTime();
 
     // if the last call to this function was more than 60 minutes ago (client was in sleep mode) reset the sync process
     if (GetTime() - lastProcess > 60 * 60) {
         Reset();
-        fBlockchainSynced = false;
+        fBlockchainSynced = true;
     }
     lastProcess = GetTime();
 
